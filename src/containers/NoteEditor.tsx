f4ae49a36139ca5c8cbd78e1686280dbba1ad20a
Tania Rascia <taniarascia@gmail.com>
2019-10-06 17:55:05 -0500

    fix weird focus issue

--- src/containers/NoteEditor.tsx
+++ src/containers/NoteEditor.tsx
@@ -36,8 +36,8 @@ const NoteEditor: React.FC<NoteEditorProps> = ({ loading, activeNote, updateNote
           updateNote({ id: activeNote.id, text: value })
         }}
         onChange={(editor, data, value) => {
-          editor.focus()
-          editor.setCursor(editor.lineCount(), 0)
+          // editor.focus()
+          // editor.setCursor(editor.lineCount(), 0)
         }}
       />
     )
