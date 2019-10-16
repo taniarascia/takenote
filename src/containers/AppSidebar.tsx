1570420c6a1457a0d54d654d404834ae5219f30c
Mudassar045 <mudassaralics007@gmail.com>
2019-10-17 00:19:13 +0500

    add check:  category name cannot empty

--- src/containers/AppSidebar.tsx
+++ src/containers/AppSidebar.tsx
@@ -94,7 +94,9 @@ const AppSidebar: React.FC = () => {
 
     const category = { id: kebabCase(tempCategory), name: tempCategory }
 
-    if (category.name.length > 20) {
+    if (category.name.trim().length === 0) {
+      setErrorCategoryMessage('Category name cannot empty')
+    } else if (category.name.length > 20) {
       setErrorCategoryMessage('Category name must not exceed 20 characters')
     } else if (categories.find(cat => cat.id === kebabCase(tempCategory))) {
       setErrorCategoryMessage('Category name has already been added')
