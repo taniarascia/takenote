33b07a19fd236e773cae2fc2cbdbbc9e46f27344
Vincent Dörig <dvincdt@gmail.com>
2019-10-19 21:23:37 +0200

    Fix exampleNote function

--- src/api/exampleNote.ts
+++ src/api/exampleNote.ts
@@ -3,27 +3,17 @@ import moment from 'moment'
 
 import markdown from '../exampleNote.md'
 
-function fetchMD() {
-  fetch(markdown)
-    .then(response => {
-      return response.text()
-    })
-    .then(text => {
-      Object.assign(exampleNote[0], { text })
-    })
-    .catch(err => {
-      throw new Error(err)
-    })
-}
-
-fetchMD()
-
-export const exampleNote = [
-  {
-    id: uuid(),
-    text: '',
-    category: '',
-    favorite: false,
-    created: moment().format(),
-  },
-]
+export const exampleNote = fetch(markdown)
+  .then(res => res.text())
+  .then(text => {
+    const exampleNote = [
+      {
+        id: uuid(),
+        text,
+        category: '',
+        favorite: false,
+        created: moment().format(),
+      },
+    ]
+    return exampleNote
+  })
