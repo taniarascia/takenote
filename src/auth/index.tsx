e8c844edc137bae967d636476a56fcc3062067b6
Tania Rascia <taniarascia@gmail.com>
2019-11-09 15:13:23 -0600

    maybe this will make a difference

--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -79,9 +79,10 @@ export const Auth0Provider = ({
     setLoading(true)
     const result = await auth0Client!.handleRedirectCallback()
     const user = await auth0Client!.getUser()
-    setLoading(false)
+
     setIsAuthenticated(true)
     setUser(user)
+    setLoading(false)
     return result
   }
   return (
