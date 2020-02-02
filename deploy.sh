1aa9619040ca5daf9d70c5cffc03733da5dcc932
Tania Rascia <taniarascia@gmail.com>
2020-02-02 17:05:58 -0600

    Fix variable in script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
+# Docker Image
 IMAGE="taniarascia/takenote"
+
+# Git Version with git hash and tags, if they exist
 GIT_VERSION=$(git describe --always --abbrev --tags --long)
 
+# Build and tag Docker image and push up to Dockerhub
 docker build -t ${IMAGE}:${GIT_VERSION} .
-docker tag ${IMAGE}:${VERSION} ${IMAGE}:latest
+docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push ${IMAGE}:${GIT_VERSION}
