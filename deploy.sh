921ceeee251c9086a654d6be3fb90a8677c2c1b5
Tania Rascia <taniarascia@gmail.com>
2020-02-02 22:15:07 -0600

    Fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,18 @@ docker build -t ${IMAGE}:${GIT_VERSION} .
 docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 
 # Login to Dockerhub and push newest build
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${IMAGE}:${GIT_VERSION}
 
 # Login to DigitalOcean command line
-doctl auth init -t "$DO_ACCESS_TOKEN" 
+doctl auth init -t "${DO_ACCESS_TOKEN}"
+
+# Decode SSH key
+echo ${DO_SSH_KEY} | base64 -d > deploy_key
+chmod 600 deploy_key
 
 # Log into Droplet, pull the latest container, and stop and start Docker
-doctl compute ssh "$DROPLET" --ssh-command docker pull ${IMAGE}:${GIT_VERSION} && 
-export CURRENT_CONTAINER=$(docker ps | grep takenote | awk '{print $1}') && 
-docker stop CURRENT_CONTAINER && 
-docker run -p 80:5000 ${IMAGE}:${GIT_VERSION}
+CONTAINER_ID=$(doctl compute ssh ${DROPLET} --ssh-key-path deploy_key --ssh-command 'docker ps | grep takenote | cut -d" " -f1')
+doctl compute ssh ${DROPLET} --ssh-key-path deploy_key --ssh-command "docker pull ${IMAGE}:${GIT_VERSION} && 
+docker stop ${CONTAINER_ID} && 
+docker run -p 80:5000 ${IMAGE}:${GIT_VERSION}"
