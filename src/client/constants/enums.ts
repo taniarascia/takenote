200095837d48299ba8f9097fe3553b44ad615ac0
Tania Rascia <taniarascia@gmail.com>
2020-02-11 17:58:44 -0600

    Update constants

--- src/client/constants/enums.ts
+++ src/client/constants/enums.ts
@@ -4,3 +4,14 @@ export enum Folder {
   FAVORITES = 'FAVORITES',
   TRASH = 'TRASH',
 }
+
+export enum Shortcuts {
+  NEW_NOTE = 'ctrl+alt+n',
+  NEW_CATEGORY = 'ctrl+alt+c',
+  DELETE_NOTE = 'ctrl+alt+u',
+  SYNC_NOTES = 'ctrl+alt+l',
+  DOWNLOAD_NOTES = 'ctrl+alt+o',
+  PREVIEW = 'alt+ctrl+p',
+  TOGGLE_THEME = 'alt+ctrl+k',
+  SEARCH = 'alt+ctrl:f',
+}
--- src/client/constants/index.ts
+++ src/client/constants/index.ts
@@ -1,10 +1,10 @@
 import { Folder } from '@/constants/enums'
 
-// `Record<Folder, string>` is preferable, but throws error `TS27411` (non-exhaustive matching)
-export const folderMap: Record<string, string> = {
+export const folderMap: Record<Folder, string> = {
   [Folder.ALL]: 'All Notes',
   [Folder.FAVORITES]: 'Favorites',
   [Folder.TRASH]: 'Trash',
+  [Folder.CATEGORY]: 'Category',
 }
 
 export const iconColor = 'rgba(255, 255, 255, 0.25)'
