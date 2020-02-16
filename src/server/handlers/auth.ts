edf9407294001e36572cc1326a04011835d2f9e6
Tania Rascia <taniarascia@gmail.com>
2020-02-16 13:02:22 -0600

    Add comments to callback endpoint

--- src/server/handlers/auth.ts
+++ src/server/handlers/auth.ts
@@ -1,32 +1,57 @@
 import { Request, Response } from 'express'
-import * as dotenv from 'dotenv'
 import axios from 'axios'
+import * as dotenv from 'dotenv'
+
+import { oneHourCookie } from '../utils'
 
 dotenv.config()
-const isProduction = process.env.NODE_ENV === 'production'
 
-const oneHourCookie = {
-  maxAge: 60 * 60 * 1000,
-  secure: isProduction,
-  httpOnly: true,
-  sameSite: true,
-}
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
 
 export default {
+  /**
+   * GitHub OAuth flow
+   * @url https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/
+   *
+   * 1. When the user hits the `/authorize` endpoint on GitHub, they
+   * will be prompted to log in (if not already logged in) and redirected to
+   * this `/callback` endpoint with a code.
+   *
+   * 2. The code will be exchanged for an access token on the `/access_token`
+   * endpoint and the user will be redirected to the app.
+   *
+   * Client-side persistance
+   * @url https://www.taniarascia.com/full-stack-cookies-localstorage-react-express/
+   *
+   * A one-hour, secure, HTTP only, and same-site cookie will be set on the app
+   * containing the access token with repo scope for accessing any GitHub data
+   * and determining login state.
+   */
   callback: async (request: Request, response: Response) => {
-    const requestToken = request.query.code
+    const { code } = request.query
 
-    const { data } = await axios({
-      method: 'post',
-      url: `https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${requestToken}`,
-      headers: {
-        accept: 'application/json',
-      },
-    })
+    try {
+      // Obtain access token
+      const { data } = await axios({
+        method: 'post',
+        url: `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`,
+        headers: {
+          accept: 'application/json',
+        },
+      })
 
-    const accessToken = data.access_token
+      const accessToken = data.access_token
 
-    response.cookie('accessToken_GitHub', accessToken, oneHourCookie)
-    response.redirect('/app')
+      // Set cookie
+      response.cookie('accessTokenGitHub', accessToken, oneHourCookie)
+
+      // Redirect to the app when logged in
+      response.redirect('/app')
+    } catch (error) {
+      console.log(error)
+      // Redirect to the main page if something went wrong
+      response.redirect('/')
+    }
   },
 }
