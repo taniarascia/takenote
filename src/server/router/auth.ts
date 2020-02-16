83f5d398227c11e28d85b8cd501402305827110b
Tania Rascia <taniarascia@gmail.com>
2020-02-16 14:09:14 -0600

    Add route for authentication

--- src/server/router/auth.ts
+++ src/server/router/auth.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import authHandler from '../handlers/auth'
+import checkAuth from '../middleware/checkAuth'
 
 const router = express.Router()
 
 router.get('/callback', authHandler.callback)
+router.get('/authenticate', checkAuth, authHandler.authenticate)
 
 export default router
