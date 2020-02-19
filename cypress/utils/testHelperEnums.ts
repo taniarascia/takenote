aabb01db4d9df28a668bad84e33b935dde66fc83
Joshua Bloom <joshbloom@google.com>
2020-02-18 21:33:02 -0800

    Moved the existing TextEnums into ResourceStringEnums

--- cypress/utils/testHelperEnums.ts
+++ cypress/utils/testHelperEnums.ts
@@ -38,24 +38,4 @@ enum TestIDEnum {
   SIDEBAR_ACTION_SYNC_NOTES = 'sidebar-action-sync-notes',
 }
 
-// Default Labels
-
-enum TextEnum {
-  ADD_CATEGORY = 'Add Category',
-  ALL_NOTES = 'All Notes',
-  CREATE_NEW_NOTE = 'Create new note',
-  DELETE_PERMANENTLY = 'Delete permanently',
-  DOWNLOAD = 'Download',
-  MARK_AS_FAVORITE = 'Mark as favorite',
-  MOVE_TO_TRASH = 'Move to trash',
-  NEW_CATEGORY = 'New category...',
-  NEW_NOTE = 'New note',
-  REMOVE_CATEGORY = 'Remove category',
-  REMOVE_FAVORITE = 'Remove favorite',
-  RESTORE_FROM_TRASH = 'Restore from trash',
-  SETTINGS = 'Settings',
-  SYNC_NOTES = 'Sync notes',
-  WELCOME_TO_TAKENOTE = 'Welcome to Takenote!',
-}
-
-export { entryPoint, dynamicTimeCategoryName, TestIDEnum, TextEnum }
+export { entryPoint, dynamicTimeCategoryName, TestIDEnum }
