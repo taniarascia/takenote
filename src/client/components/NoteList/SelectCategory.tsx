8bd3c671df0679f6c1e34c2046c7159ad21d489d
G-Milevski <georg.milevski@gmail.com>
2020-04-14 06:03:51 +0300

    Fixed select of category (#337)
    * select of category is moved to separate component.
    
    * Fixed bug if a note relat to category then in context menu of the note
    should not be a select of category.
    
    Co-authored-by: Tania Rascia <taniarascia@gmail.com>

---
+++ src/client/components/NoteList/SelectCategory.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { TestID } from '@resources/TestID'
+
+import { NoteItem, CategoryItem, NoteState } from '@/types'
+
+export interface SelectCategoryProps {
+  onChange: (selectedOption: any) => void
+  categories: CategoryItem[]
+  note: NoteItem
+  activeCategoryId: string
+}
+
+export const SelectCategory: React.FC<SelectCategoryProps> = ({
+  onChange,
+  categories,
+  note,
+  activeCategoryId,
+}) => {
+  const filteredCategories = categories
+    .filter(({ id }) => id !== activeCategoryId)
+    .filter((category) => category.id !== note.category)
+  return (
+    <>
+      {!note.trash && filteredCategories.length > 0 && (
+        <select
+          data-testid={TestID.MOVE_TO_CATEGORY}
+          defaultValue=""
+          className="nav-item move-to-category-select"
+          onChange={onChange}
+        >
+          <option disabled value="">
+            Move to category...
+          </option>
+          {filteredCategories
+            .filter((category) => category.id !== note.category)
+            .map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+        </select>
+      )}
+    </>
+  )
+}
