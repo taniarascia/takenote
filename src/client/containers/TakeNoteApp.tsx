8fdc1e91c13555e2c6b89695c1f49f6bdb22e80f
Tania Rascia <tania.rascia@yum.com>
2020-10-17 20:06:36 -0500

    Max resize

--- src/client/containers/TakeNoteApp.tsx
+++ src/client/containers/TakeNoteApp.tsx
@@ -88,9 +88,9 @@ export const TakeNoteApp: React.FC = () => {
       <TempStateProvider>
         <div className={determineAppClass(darkTheme, sidebarVisible, activeFolder)}>
           <DragDropContext onDragEnd={onDragEnd}>
-            <SplitPane split="vertical" minSize={150} defaultSize={240}>
+            <SplitPane split="vertical" minSize={150} maxSize={500} defaultSize={240}>
               <AppSidebar />
-              <SplitPane split="vertical" minSize={200} defaultSize={340}>
+              <SplitPane split="vertical" minSize={200} defaultSize={340} maxSize={600}>
                 <NoteList />
                 <NoteEditor />
               </SplitPane>
