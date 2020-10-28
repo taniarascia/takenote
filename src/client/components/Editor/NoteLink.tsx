2c3248abb872fe332e45773aae9011021dec1741
Kody Clemens <kody@kodyclemens.com>
2020-10-28 16:57:24 -0500

    More work on linked notes - Notes are now properly "linked" - Clicking on a link with load a new active note in the editor

--- src/client/components/Editor/NoteLink.tsx
+++ src/client/components/Editor/NoteLink.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 import { NoteItem } from '@/types'
+import { updateActiveNote, updateSelectedNotes, pruneNotes } from '@/slices/note'
 
 import { getNoteTitle, getActiveNoteFromShortUuid } from '../../utils/helpers'
 
@@ -10,11 +12,31 @@ export interface NoteLinkProps {
 }
 
 const NoteLink: React.FC<NoteLinkProps> = ({ notes, uuid }) => {
+  const dispatch = useDispatch()
+
+  const _updateSelectedNotes = (noteId: string, multiSelect: boolean) =>
+    dispatch(updateSelectedNotes({ noteId, multiSelect }))
+
+  const _updateActiveNote = (noteId: string, multiSelect: boolean) =>
+    dispatch(updateActiveNote({ noteId, multiSelect }))
+
+  const _pruneNotes = () => dispatch(pruneNotes())
+
   const note = getActiveNoteFromShortUuid(notes, uuid)
   const title = note !== undefined ? getNoteTitle(note.text) : null
-  const notFoundErrorMsg = '<linked note not found>'
+  const notFoundErrorMsg = '<invalid note uuid provided>'
 
-  if (title) return <a href="#">{title}</a>
+  const handleClick = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+
+    if (note) {
+      _updateActiveNote(note.id, false)
+      _updateSelectedNotes(note.id, false)
+      _pruneNotes()
+    }
+  }
+
+  if (title) return <a onClick={handleClick}>{title}</a>
 
   return <span className="error">{notFoundErrorMsg}</span>
 }
