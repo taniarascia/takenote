c74f0362c2884cc19560e7d4f1e7d41ace81d260
Dusabe Johnson <59834399+joshwambere@users.noreply.github.com>
2020-12-12 00:11:33 +0200

    implementing tests for previewEditor component (#475) [skip ci]

--- src/client/components/Editor/NoteLink.tsx
+++ src/client/components/Editor/NoteLink.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { NoteItem } from '@/types'
 import { Errors } from '@/utils/enums'
+import { TestID } from '@resources/TestID'
 
 import { getNoteTitle, getActiveNoteFromShortUuid } from '../../utils/helpers'
 
@@ -15,9 +16,18 @@ const NoteLink: React.FC<NoteLinkProps> = ({ notes, uuid, handleNoteLinkClick })
   const note = getActiveNoteFromShortUuid(notes, uuid)
   const title = note !== undefined ? getNoteTitle(note.text) : null
 
-  if (note && title) return <a onClick={(e) => handleNoteLinkClick(e, note)}>{title}</a>
+  if (note && title)
+    return (
+      <a data-testid={TestID.NOTE_LINK_SUCCESS} onClick={(e) => handleNoteLinkClick(e, note)}>
+        {title}
+      </a>
+    )
 
-  return <span className="error">{Errors.INVALID_LINKED_NOTE_ID}</span>
+  return (
+    <span data-testid={TestID.NOTE_LINK_ERROR} className="error">
+      {Errors.INVALID_LINKED_NOTE_ID}
+    </span>
+  )
 }
 
 export default NoteLink
