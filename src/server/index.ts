c9448317bca0020cde17d43431a7cdc2952edd35
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-05-31 03:13:14 +0500

    issue with packages resolved

--- src/server/index.ts
+++
@@ -1,6 +0,0 @@
-import initializeServer from './initializeServer'
-import router from './router'
-
-const app = initializeServer(router)
-
-app.listen(5000, () => console.log(`Listening on port ${5000}`)) // eslint-disable-line
