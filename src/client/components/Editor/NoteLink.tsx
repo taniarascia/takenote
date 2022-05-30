5190f594c032272b1cd3cb9f10120d1691e07e2d
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-05-31 03:46:24 +0500

    note link component added

---
+++ src/client/components/Editor/NoteLink.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { NoteItem } from '@/types'
+import { Errors } from '@/utils/enums'
+import { TestID } from '@resources/TestID'
+
+// import { getNoteTitle, getActiveNoteFromShortUuid } from '../src/client/utils/helpers'
+
+export interface NoteLinkProps {
+  uuid: string
+  notes: NoteItem[]
+  handleNoteLinkClick: (e: React.SyntheticEvent, note: NoteItem) => void
+}
+
+const NoteLink: React.FC<NoteLinkProps> = ({ notes, uuid, handleNoteLinkClick }) => {
+  // const note = getActiveNoteFromShortUuid(notes, uuid)
+  // const title = note !== undefined ? getNoteTitle(note.text) : null
+  const note = 'note'
+  const title = 'title'
+  if (note && title)
+    return (
+      <a data-testid={TestID.NOTE_LINK_SUCCESS} onClick={(e) => handleNoteLinkClick(e, _)}>
+        {title}
+      </a>
+    )
+
+  return (
+    <span data-testid={TestID.NOTE_LINK_ERROR} className="error">
+      {Errors.INVALID_LINKED_NOTE_ID}
+    </span>
+  )
+}
+
+export default NoteLink
