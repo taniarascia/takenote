79575a6467a7ee70f5b44a0873c60ad44d6556a2
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-05-31 03:32:06 +0500

    basic seach bar component added

--- src/client/components/NoteList/SearchBar.tsx
+++ src/client/components/NoteList/SearchBar.tsx
@@ -1,26 +1,7 @@
 import React from 'react'
 
-import { TestID } from '@resources/TestID'
-
-export interface SearchBarProps {
-  searchRef: React.MutableRefObject<HTMLInputElement>
-  searchNotes: (searchValue: string) => void
+const SearchBar = () => {
+  return <div>SearchBar</div>
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({ searchRef, searchNotes }) => {
-  return (
-    <input
-      ref={searchRef}
-      data-testid={TestID.NOTE_SEARCH}
-      type="search"
-      onChange={(event) => {
-        event.preventDefault()
-        searchNotes(event.target.value)
-      }}
-      placeholder="Search for notes"
-      onDragOver={(e) => {
-        e.preventDefault()
-      }}
-    />
-  )
-}
+export default SearchBar
